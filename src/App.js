f9fdfcd2f2175e5830e8c4c2024ef82c778bae7b
drashti04 <85118452+drashti-thekdi@users.noreply.github.com>
2025-07-01 11:24:03 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -5,21 +5,21 @@ import Home from "./pages/Home";
 import Menu from "./pages/Menu";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Router>
+      <BrowserRouter>
         <Navbar />
         <Routes>
            <Route path="/"  element={<Home />} />
-          <Route path="/menu" element={<Menu />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
+          <Route path="/menu"  element={<Menu/>} />
+          <Route path="/about"  element={<About/>} />
+          <Route path="/contact"  element={<Contact/>} />
         </Routes>
         <Footer />
-      </Router>
+      </BrowserRouter>
     </div>
   );
 }
