057c5334a0db56dfdfd8a961e0620a9e6169515f
drashti-thekdi <85118452+drashti-thekdi@users.noreply.github.com>
2025-07-01 16:31:37 +0530

    comit App.js

--- src/App.js
+++ src/App.js
@@ -10,17 +10,22 @@ import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-           <Route path="/"  element={<Home />} />
-          <Route path="/menu"  element={<Menu/>} />
-          <Route path="/about"  element={<About/>} />
-          <Route path="/contact"  element={<Contact/>} />
-          <Navigate to="/" />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
+       <BrowserRouter>
+      <Navbar />
+
+      <Routes>
+        {/* default homepage */}
+        <Route path="/" element={<Home />} />
+        {/* other pages */}
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        {/* catch-all fallback redirects to homepage */}
+        <Route path="*" element={<Home />} />
+      </Routes>
+
+      <Footer />
+    </BrowserRouter>
     </div>
   );
 }
